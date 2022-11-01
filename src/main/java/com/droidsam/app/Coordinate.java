3b594e6edfe223d30fef9f5a9f0779bc74312025
droidsam1 <droidsam@gmail.com>
2022-11-01 08:18:25 +0100

    game remember players moves

--- src/main/java/com/droidsam/app/Coordinate.java
+++ src/main/java/com/droidsam/app/Coordinate.java
@@ -1,11 +1,24 @@
 package com.droidsam.app;
 
 public class Coordinate {
+
+    private final int x;
+    private final int y;
+
+    private Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Coordinate of(int x, int y) {
+        return new Coordinate(x, y);
+    }
+
     public int getX() {
-        return 0;
+        return x;
     }
 
     public int getY() {
-        return 0;
+        return y;
     }
 }
--- src/test/java/com/droidsam/app/TicTacToeGameTest.java
+++ src/test/java/com/droidsam/app/TicTacToeGameTest.java
@@ -22,8 +22,22 @@ public class TicTacToeGameTest {
     @Test
     public void aPlayerCanPlace() {
         Player player = new Player();
-        Coordinate position = new Coordinate();
+        Coordinate position = Coordinate.of(0, 0);
         assertDoesNotThrow(() -> game.place(player, position));
         assertEquals(player, game.getPlayerAtPosition(position));
+    }
+
+    @Test
+    public void gameRememberPlayersMoves() {
+        Player player = new Player();
+        Coordinate position = Coordinate.of(0, 0);
+        Player anotherPlayer = new Player();
+        Coordinate anotherPosition = Coordinate.of(1, 1);
+
+        assertDoesNotThrow(() -> game.place(player, position));
+        assertDoesNotThrow(() -> game.place(anotherPlayer, anotherPosition));
+
+        assertEquals(player, game.getPlayerAtPosition(position));
+        assertEquals(anotherPlayer, game.getPlayerAtPosition(anotherPosition));
     }
 }
